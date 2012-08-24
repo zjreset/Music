15618d3cda7a3b1a3e2bd8fb4a4cd818a7c625c3
runes <runes@runes-MacBook-Pro.local>
2012-08-24 16:47:41 +0800

    Initial Commit

---
+++ Music/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Music
+//
+//  Created by runes on 12-8-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
